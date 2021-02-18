b847c1a3efb06ba9f9e4d3556aa2feb2b3042728
Angel Leung <angelhleung@gmail.com>
2021-02-18 15:13:07 -0800

    Used the CSS transform property to have the image of the car rotate with the keydown event. #2

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 function pressKey(event) {
+  var turn = null;
   if (event.key === 'ArrowLeft') {
-    return event.key;
+    turn = -1;
   } else if (event.key === 'ArrowRight') {
-    return event.key;
+    turn = 1;
+  } else {
+    return;
   }
+  var currentDirectionIndex = directions.indexOf(currentDirection);
+  var newDirectionIndex = currentDirectionIndex + turn;
+  if (newDirectionIndex < 0) {
+    newDirectionIndex = 3;
+  } else if (newDirectionIndex > 3) {
+    newDirectionIndex = 0;
+  }
+  currentDirection = directions[newDirectionIndex];
+  $car.className = 'car ' + currentDirection;
 }
 
-// var $car = document.querySelector('.car');
+var $car = document.querySelector('.car');
 document.addEventListener('keydown', pressKey);
+
+var directions = ['east', 'south', 'west', 'north'];
+var currentDirection = 'east';
